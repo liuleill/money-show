38da71d0d72bcda8c0c6130dac55ab505a2f1fbd
刘磊 <2413882532@qq.com>
2021-06-18 17:18:00 +0800

    deploy

---
+++ precache-manifest.d354c43311e2f02775fa3242cd1a5509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc88d2f486bb555042ad",
+    "url": "/money-show/css/app.dcc80162.css"
+  },
+  {
+    "revision": "1a05b8f7bc6c835a9144155f37d8ad27",
+    "url": "/money-show/index.html"
+  },
+  {
+    "revision": "dc88d2f486bb555042ad",
+    "url": "/money-show/js/app.6161cbab.js"
+  },
+  {
+    "revision": "cee5de9fcf1612835ce3",
+    "url": "/money-show/js/chunk-vendors.d04fceb4.js"
+  },
+  {
+    "revision": "67e889503ab8e3c0e0f04cd0163755f1",
+    "url": "/money-show/manifest.json"
+  },
+  {
+    "revision": "c424656d51a3ad7d8551c3f681654750",
+    "url": "/money-show/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-show/robots.txt"
+  }
+]);
